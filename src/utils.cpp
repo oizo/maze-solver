3e752c8d49007006f7e8e068eda5c2470181704c
Danny Hvam <dkhvam@gmail.com>
2023-03-15 22:19:55 +0100

    Add colors to render

--- src/utils.cpp
+++ src/utils.cpp
@@ -15,8 +15,20 @@ namespace maze
         ", exit=" + std::to_string(maze.exit) + " ]";
     }
 
+    void color(std::string &result, const int &color, const char &c) {
+        result += "\033[";
+        result += std::to_string(color);
+        result += ";1m";
+        result += c;
+        result += "\033[0m";
+    }
+
+    void noColor(std::string &result, const char &c) {
+        result += c;
+    }
+
     std::string render(const Maze &maze, const RenderConfig &rc) {
-        
+
         std::string result;
 
         auto grid = maze.grid;
@@ -25,7 +37,7 @@ namespace maze
         int width = (maze.width * 2) + 1;
         int entry = maze.entry * 2;
         std::string entryIndicator(entry - 1, rc.empty);
-        entryIndicator += rc.entry;
+        color(entryIndicator, rc.color.path, rc.entry);
         entryIndicator.append(width - entry, rc.empty);
         result += entryIndicator + "\n";
 
@@ -35,33 +47,47 @@ namespace maze
             std::string botttom;
             for (int x = 0; x < grid->at(y)->size(); ++x) {
                 auto *c = grid->at(y)->at(x);
-                top += rc.corner;
-                top += contains(c->walls, Direction::NORTH) ? rc.wallH : rc.empty;
-                botttom += contains(c->walls, Direction::WEST) ? rc.wallV : rc.empty;
-                botttom += c->entry ? (c->exit ? rc.solution : rc.visited) : rc.empty;
+                noColor(top, rc.corner);
+                // Draw horizontal walls
+                contains(c->walls, Direction::NORTH)
+                    ? noColor(top, rc.wallH)
+                    : color(top, rc.color.empty, rc.empty);
+                // Draw vertical walls
+                contains(c->walls, Direction::WEST)
+                    ? noColor(botttom, rc.wallV)
+                    : color(botttom, rc.color.empty, rc.empty);
+                // Draw the cell
+                c->entry
+                    ? c->exit
+                        ? color(botttom, rc.color.path, rc.path)
+                        : color(botttom, rc.color.visited, rc.visited)
+                    : color(botttom, rc.color.empty, rc.empty);
+
             }
-            top += rc.corner;
-            botttom += rc.wallV;
+            noColor(top, rc.corner);
+            noColor(botttom, rc.wallV);
             result += top + "\n";
             result += botttom + "\n";
         }
 
         // Build bottom row of maze
         std::string bottomRow;
-        bottomRow += rc.corner;
+        noColor(bottomRow, rc.corner);
         for (auto cell = grid->back()->begin(); cell != grid->back()->end(); ++cell) {
-            bottomRow += contains((*cell)->openings, Direction::SOUTH) ? rc.empty : rc.wallH;
-            bottomRow += rc.corner;
+            contains((*cell)->openings, Direction::SOUTH)
+                ? color(bottomRow, rc.color.empty, rc.empty)
+                : noColor(bottomRow, rc.wallH);
+            noColor(bottomRow, rc.corner);
         }
         result += bottomRow + "\n";
 
         // Build exit indicator
         int exit = maze.exit * 2;
         std::string exitIndicator(exit - 1, rc.empty);
-        exitIndicator += rc.exit;
+        color(exitIndicator, rc.color.path, rc.exit);
         exitIndicator.append(width - exit, rc.empty);
         result += exitIndicator;
-        
+
         return result;
     }
 }
--- src/utils.h
+++ src/utils.h
@@ -4,15 +4,23 @@
 #include "src/cell.h"
 
 namespace maze {
+
+    struct RenderColor {
+        int path = 93;
+        int visited = 90;
+        int empty = 97;
+    };
+
     struct RenderConfig {
         char corner = '+';
         char wallH = '-';
         char wallV = '|';
-        char visited = '.';
-        char solution = '#';
+        char visited = '*';
+        char path = '*';
         char empty = ' ';
         char entry = 'v';
         char exit = 'v';
+        RenderColor color;
     };
     
     std::string info(const Maze &maze);
